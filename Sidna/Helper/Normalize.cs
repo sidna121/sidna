fe0145f08a3ff81a902d55695478ffdca76ab959
Kaveh Mohammadi <k.mohammadi.d.1366@gmail.com>
2021-08-02 09:44:50 +0430

    Normalize fix

--- Sidna/Helper/Normalize.cs
+++ Sidna/Helper/Normalize.cs
@@ -42,41 +42,34 @@ namespace Sidna.Helper
         public static List<History> histiryType(History fromHistory, List < History> list, double Range)
         {
             //     ***  refactor  ***
+            List<History> list2 = new List<History>();
             List<History> returnList = new List<History>();
-            foreach (var history in list)
+
+            for (int i = 0; i < list.Count; i++)
             {
-                if (fromHistory != null)
+                var h = new History { ID = list[i].ID};
+                if (i > 0)
                 {
-                    if(fromHistory.Type == HistoryType.صعودی)
-                    {
-                        if (history.Type == HistoryType.نزولی
-                            && fromHistory.End > (history.End + Range))
-                        {
-                            fromHistory = history;
-                            int i7 = list.FindIndex(x => x.ID == history.ID);
-                            for (int i = list.FindIndex(x => x.ID == history.ID); i >= 0; i--)
-                                if (list[i].Type == fromHistory.Type && list[i].Str > fromHistory.Str)
-                                    fromHistory = list[i];
-                            returnList.Add(fromHistory);
-                        }
-                    }
-                    else if(fromHistory.Type == HistoryType.نزولی)
-                    {
-                        if (history.Type == HistoryType.صعودی
-                            && fromHistory.End < (history.End - Range))
-                        {
-                            fromHistory = history;
-                            for (int i = list.FindIndex(x => x.ID == history.ID); i >= 0; i--)
-                                if (list[i].Type == fromHistory.Type &&  list[i].Str < fromHistory.Str)
-                                    fromHistory = list[i];
-                            returnList.Add(fromHistory);
-                        }
-                    }
-                    else
-                        fromHistory = history;
+                    if (list[i].End > (fromHistory.End + Range) || list[i].End < (fromHistory.End - Range))
+                        h.End = list[i].End;
+                    else h.End = fromHistory.End;
                 }
-                else
-                    fromHistory = history;
+                else h.End = list[i].End;
+
+                list2.Add(h);
+
+                fromHistory = list[i];
+                fromHistory.End = h.End;
+            }
+
+            returnList.Add(list[0]);
+            foreach (var h in list2)
+            {
+                if (fromHistory.End != h.End)
+                {
+                    returnList.Add(h);
+                    fromHistory = h;
+                }
             }
             return returnList;
         }
